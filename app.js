12b60db53ba28dcab09e9819b0098a3bef6b3dbd
Marwane <manbou92@hotmail.fr>
2021-07-08 12:15:12 +0200

    Ajout de la librairie stopword by JIOVANNY

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const stopword = require('stopword');
+
 const Twit = require("twit");
-const fs = require('fs/promises');
+const fs = require('fs');
 require("dotenv").config();
 const elasticbulk = require('elasticbulk');
 const twitter = [];
+
+const wordEmpty = fs.readFileSync('wordEmpty.txt');
 
 const T = new Twit({
   consumer_key: process.env.consumer_key,
@@ -12,15 +16,17 @@ const T = new Twit({
 });
 
 const getAllData = (_) => {
-  T.get('search/tweets', { q: '#covid19 OR #COVID19 OR #covid-19',lang: "fr", tweet_mode: "extended", count:100 }, function getData(err, data, response) {
+  T.get('search/tweets', { q: '#Vaccin OR #Covid19 OR #VACCIN OR #COVID19 OR #vaccination #vaccins #vaccinationObligatoire OR #ObligationVaccination' ,lang: "fr", tweet_mode: "extended", count:1000 }, function getData(err, data, response) {
     data.statuses.forEach((item) => {
+      const oldString = (item.retweeted_status !== undefined ? item.retweeted_status.full_text : item.full_text).split(' ');
       twitter.push({
+        "tweet" : (stopword.removeStopwords(oldString, wordEmpty.toString().split('\n'))).join(' '),
         "id": item.id,
-        "tweet" : item.retweeted_status !== undefined ? item.retweeted_status.full_text : item.text,
         "created_at": item.created_at,
         "user": item.user.screen_name
       });
     });
+    console.log(twitter);
     elasticbulk.import(twitter,{
       index: 'logs_server_elk_esgi',
       type: 'TP',
